4a8e5539e36554ac04667f5759d47b271af4560b
Ahmed Senousy <ahmedsenousy01@gmail.com>
2023-04-16 11:52:20 +0200

    refactor: :construction: completely refactored the project
    used a different structure (mvc), added some endpoints, etc...

--- utils/dbConnection.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-module.exports = () => {
-  mongoose.connect('mongodb://localhost:27017/login-and-registration')
-    .then(() => console.log('connection established'))
-    .catch(err => console.log(err));
-}
